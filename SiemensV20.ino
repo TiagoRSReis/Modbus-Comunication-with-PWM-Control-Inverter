d387d193f235ce8b601623d1d64f78f80a75a56a
TiagoRSReis <58647525+TiagoRSReis@users.noreply.github.com>
2021-06-03 01:18:29 +0100

    Update SiemensV20.ino

--- SiemensV20.ino
+++ SiemensV20.ino
@@ -1,28 +1,28 @@
 #include <ModbusMaster.h>               //Library for using ModbusMaster
 #include <SoftwareSerial.h>             //Library for using additional serial port
 
-#define MAX485_DE      5
-#define MAX485_RE_NEG  16
-#define Adress_to_read_speed  23 //Freq,Speed,current,torque (Hz,RPM,A,Nm)
-#define Adress_to_read_erros  54 //Erros
-#define Quantity_of_bits  4
-#define Quantity_of_bits_for_error  4
+#define MAX485_DE      5                // DE pin in MAX485
+#define MAX485_RE_NEG  16               // RE pin in MAX485
+#define Adress_to_read_speed  23        //Adress for values of Freq,Speed,current,torque (Hz,RPM,A,Nm)
+#define Adress_to_read_erros  54        //Adress for values of Erros
+#define Quantity_of_bits  4             //Quantity of values to read
+#define Quantity_of_bits_for_error  4   //Quantity of erros to read
 
 //Config for internet connection and Mqtt publisher
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-WiFiClient TCP_Client; // Objecto do tipo TCP
-PubSubClient client(TCP_Client); // Objecto do tipo cliente MQTT
-String Network_name ="MEO-661930";
-String Network_pass ="dfb8882917";
-IPAddress Ip_Broker_Mqtt(192, 168, 1, 101);
-int Port_Broker = 1883;
+WiFiClient TCP_Client;                                         // Object type tipo TCP
+PubSubClient client(TCP_Client);                               // Object type client MQTT
+String Network_name ="put here your SSID";
+String Network_pass ="put here your network password";
+IPAddress Ip_Broker_Mqtt(192, 168, 1, 101);                    //IP of broker MQTT
+int Port_Broker = 1883;                                        //MQTT Port 
 
-SoftwareSerial mySerial(4, 0); //software serial 12->Rx 13->Tx 
+SoftwareSerial mySerial(4, 0);                                 //software serial 4->Rx 0->Tx 
 
-ModbusMaster node;                    //object node for class ModbusMaster
+ModbusMaster node;                                             //object node for class ModbusMaster
 
-int Delay_on_acquisition = 2000;
+int Delay_on_acquisition = 2000;                               //Time between read all over again 
 
 //--------------------------------END OF CONFIGS,INCLUDES,BLABLABLA-----------------------------------------
 void preTransmission()            //Function for setting stste of Pins DE & RE of RS-485
@@ -42,36 +42,35 @@ void setup()
   Serial.begin(9600);             //Baud Rate of Serial as 115200 
   mySerial.begin(9600);           //Baud Rate of SoftwareSerial as 115200 
   
-//   // WiFi network
-//  Serial.println(); Serial.print("Connecting to ");Serial.println(Network_name);
-//  WiFi.mode(WIFI_STA);
-//  WiFi.begin(Network_name, Network_pass);
-//  while (WiFi.status() != WL_CONNECTED) {
-//  delay(500); Serial.print(".");}
-//  Serial.println();
-//  Serial.print("The ESP is configurated with this ip: ");
-//  Serial.println(WiFi.localIP());
+   // WiFi network
+  Serial.println(); Serial.print("Connecting to ");Serial.println(Network_name);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(Network_name, Network_pass);
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(500); Serial.print(".");}
+  Serial.println();
+  Serial.print("The ESP is configurated with this ip: ");
+  Serial.println(WiFi.localIP());
 
  // Config the GPIOS to ModBus 
   pinMode(MAX485_RE_NEG, OUTPUT);
   pinMode(MAX485_DE, OUTPUT);
-//  digitalWrite(MAX485_RE_NEG, 0);
-//  digitalWrite(MAX485_DE, 0);
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 1);
 
-Serial.println("max config");
 
 
   // Config for ModBusMaster
-  node.begin(1, mySerial);            //Slave ID as 1
+  node.begin(1, mySerial);                       //Slave ID as 1
   node.preTransmission(preTransmission);         //Callback for configuring RS-485 Transreceiver correctly
-  node.postTransmission(postTransmission);      //Callback for configuring RS-485 Receive correctly
+  node.postTransmission(postTransmission);       //Callback for configuring RS-485 Receive correctly
 
  Serial.println("modbus configured");
 
-//    // MQTT
-// client.setServer(Ip_Broker_Mqtt, Port_Broker); // The MQTT, Broker, stay on pc “192.168.1.2”
-// // client.setCallback(callback);
-// client.connect("ESP8266"); // O ESP need to register in broker. For any new equipment new name
+    // MQTT
+ client.setServer(Ip_Broker_Mqtt, Port_Broker);       
+ client.setCallback(callback);
+ client.connect("ESP8266");                           // O ESP need to register in broker. For any new equipment new name
 }
 
 void loop()
@@ -133,26 +132,26 @@ if (result == node.ku8MBSuccess)
     if(j==0){
     Serial.print("Last Fault = ");
     Serial.println(data[j]);
-//    data[j].toCharArray(data_mqtt,16);
-//    client.publish("last_fault", data_mqtt);
+    data[j].toCharArray(data_mqtt,16);
+    client.publish("last_fault", data_mqtt);
     }
     if(j==1){
     Serial.print("1. Fault = ");
     Serial.println(data[j]);
-//    data[j].toCharArray(data_mqtt,16);
-//    client.publish("1.fault", data_mqtt);
+    data[j].toCharArray(data_mqtt,16);
+    client.publish("1.fault", data_mqtt);
     }
     if(j==2){
     Serial.print("2. Fault = ");
     Serial.println(data[j]);
-//    data[j].toCharArray(data_mqtt,16);
-//    client.publish("2.fault", data_mqtt);
+    data[j].toCharArray(data_mqtt,16);
+    client.publish("2.fault", data_mqtt);
     }
     if(j==3){
     Serial.print("3. Fault = ");
     Serial.println(data[j]);
-//    data[j].toCharArray(data_mqtt,16);
-//    client.publish("3.fault", data_mqtt);
+    data[j].toCharArray(data_mqtt,16);
+    client.publish("3.fault", data_mqtt);
     }
 }
 }
@@ -163,14 +162,3 @@ delay(Delay_on_acquisition);
 }
 }
 
-
-//  for (j = 0; j < Quantity_of_bits; j++)
-//  {
-//    data[j] = node.getResponseBuffer(j);
-//    if(j==0){
-//    Serial.print("Data presente na posição ");Serial.print(Adress_to_read);
-//    Serial.print(" e consecutivas ");Serial.print(Quantity_of_bits);
-//    Serial.println(" posições"); Serial.print(j);
-//    }
-//    Serial.println(data[j]);
-//  }
